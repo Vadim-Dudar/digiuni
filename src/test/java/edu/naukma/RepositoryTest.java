ee9c4223168b87b3eda3ef2ffa7c567649277963
Vadim Dudar <v.dudar@ukma.edu.ua>
2026-03-11 23:41:14 +0200

    feat: add RepositoryTest class with unit tests for addItem and remove methods

---
+++ src/test/java/edu/naukma/RepositoryTest.java
@@ -0,0 +1,53 @@
+package edu.naukma;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeClass implements Identifiable {
+    private final int id;
+    String name;
+
+    public SomeClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+class RepositoryTest {
+
+    Repository<SomeClass> repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new Repository<>();
+        repository.addItem(new SomeClass(1, "Item 1"));
+        repository.addItem(new SomeClass(2, "Item 2"));
+    }
+
+    @Test
+    @DisplayName("Test addItem method of Repository with null item")
+    void addNullItem() {
+        assertThrows(IllegalArgumentException.class, () -> repository.addItem(null));
+    }
+
+    @Test
+    @DisplayName("Test addItem method of Repository with valid item")
+    void addItem() {
+        SomeClass newItem = new SomeClass(3, "Item 3");
+        repository.addItem(newItem);
+        assertTrue(repository.getAll().contains(newItem));
+    }
+
+    @Test
+    @DisplayName("Test remove method of Repository")
+    void remove() {
+        assertTrue(repository.remove(1));
+    }
+}
